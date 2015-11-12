1a7be4fa4de7a367d55e617e4808efc9e011a714
Dean Sinclair <Dean Sinclair>
2015-11-12 01:21:04 +0000

    Further improved touch input, adding simple braking support and analogue/variable steering.

--- MonoGame-UI-Touch-Prototype/Game1.cs
+++ MonoGame-UI-Touch-Prototype/Game1.cs
@@ -17,9 +17,14 @@ namespace MonoGame_UI_Touch_Prototype
 
 		TouchCollection touchC;
 
-		Texture2D texture;
+		Texture2D carTexture;
 		Sprite car;
 		float speed = 5;
+		bool brake = false;
+
+        Texture2D circleTexture;
+        Sprite circle;
+        Vector2 circleStart;
 		
 
         public Game1()
@@ -55,8 +60,12 @@ namespace MonoGame_UI_Touch_Prototype
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			texture = Content.Load<Texture2D>("car");
-			car = new Sprite(texture, new Vector2(200, 200), 0);
+			carTexture = Content.Load<Texture2D>("car");
+			car = new Sprite(carTexture, new Vector2(200, 200), 0);
+
+            circleTexture = Content.Load<Texture2D>("circle");
+            circleStart = new Vector2(GraphicsDevice.DisplayMode.Width/2 - circleTexture.Width/2, GraphicsDevice.DisplayMode.Height/2 - circleTexture.Height/2);
+            circle = new Sprite(circleTexture, circleStart, 0);
             // TODO: use this.Content to load your game content here
         }
 
@@ -79,38 +88,56 @@ namespace MonoGame_UI_Touch_Prototype
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-			//car.SetRotation(car.GetRotationDegrees() + 0.05f);
 			touchC = TouchPanel.GetState();
-			bool left = false;
-			bool right = false;
 
+            bool set = false;
+			bool brake = false;
+
+			// Get touch
 			foreach (TouchLocation tl in touchC) {
-				if (tl.Position.X < GraphicsDevice.DisplayMode.Width / 2) {
-					left = true;
+				if (set && !brake) {		// touch 2
+					brake = true;
 				}
-				if (tl.Position.X > GraphicsDevice.DisplayMode.Width / 2) {
-					right = true;
+				if (!set) {					// touch 1
+                    circle.SetPosition(new Vector2(tl.Position.X - circleTexture.Width / 2, tl.Position.Y - circleTexture.Height / 2));
+                    set = true;
+                }
+			}
+
+
+			// If no active touch
+            if (!set) {
+                circle.SetPosition(circleStart);
+            }
+
+			// Set rotation according to how far the touch is from the centre, if car is moving
+			if (speed > 0.5) {
+				float turn = (circleStart.X - circle.GetPosition().X) / 200 * -1;
+
+				car.SetRotation(car.GetRotationDegrees() + turn);
+			}
+
+			// Check if car is braking
+			if (brake) {
+				if (speed > 0) {
+					speed -= 0.1f;
+				} else if (speed < 0) {
+					speed = 0;
+				}
+			} else {
+				if (speed < 5) {
+					speed += 0.1f;
+				}else if(speed > 5){
+					speed = 5;
 				}
 			}
 
-			float turn = 0;
-
-			if (left) {
-				turn -= 1;
-			}
-			if (right) {
-				turn += 1;
-			}
-
-			car.SetRotation(car.GetRotationDegrees() + turn);
-
-			// move forward
-
+			// Move car forward by the speed
             Vector2 direction = new Vector2((float)Math.Cos(car.GetRotationRadians()), (float)Math.Sin(car.GetRotationRadians()));
 
             direction.Normalize();
 
-            car.SetPosition(car.GetPosition() + direction * 2);
+            car.SetPosition(car.GetPosition() + direction * speed);
 
 
 			// TODO: Add your update logic here
@@ -129,7 +156,8 @@ namespace MonoGame_UI_Touch_Prototype
             // TODO: Add your drawing code here
 			spriteBatch.Begin();
 			spriteBatch.Draw(car.GetTexture(), car.GetPosition(), rotation: car.GetRotationRadians(), origin: new Vector2(car.GetTexture().Width / 2, car.GetTexture().Height / 2));
-			spriteBatch.End();
+            spriteBatch.Draw(circle.GetTexture(), circle.GetPosition());
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
